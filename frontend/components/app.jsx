31106e425ef0f6b3bb751670c1a038834690b8af
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2022-01-24 17:50:20 -0600

    push to heroku

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,6 +4,7 @@ import Greetingcontainer from './greeting/Greeting_container';
 import LoginFormContainer from './sessionForm/Login_container'
 import SignupFormContainer from './sessionForm/Signup_container';
 import { AuthRoute } from '../util/route_util';
+// 'hello'
 const App = ()=>(
     <div>
         <Switch>
