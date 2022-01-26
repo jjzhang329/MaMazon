2b201b2026f200382fdac5390b182a7741fe1d05
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2022-01-25 18:58:27 -0600

    fixing heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+User.destroy_all
 demo = User.create!({name: 'Demo', email:"plshireme@company.com", password:"password"})
