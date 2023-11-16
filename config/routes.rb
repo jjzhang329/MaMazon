1a6f48453c27ba40599149cf493fd9c1bcb554f0
Jing Jing Zhang <ariel_jingjingzhang@hotmail.com>
2023-11-16 12:18:36 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#root"
+  namespace :api, defaults: {format: :json} do
+      resources :users, only:[:create]
+      resource :session, only:[:create, :destroy]
+      resources :products, only:[:index, :show]
+      resources :carts, only:[:index, :create, :update, :destroy]
+      resources :reviews, only:[:create, :update, :destroy]
+  end
 end
